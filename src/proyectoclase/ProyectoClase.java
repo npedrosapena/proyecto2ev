590dae4e2f5671fcbcc3ee88e4134c4497ada291
findag <findag@findag-TOSH>
2014-03-17 10:51:25 +0100

    Añadimos clase menu

--- src/proyectoclase/ProyectoClase.java
+++ src/proyectoclase/ProyectoClase.java
@@ -19,6 +19,8 @@ public class ProyectoClase
     public static void main(String[] args)
     {
         // TODO code application logic here
+        Menu menu = new Menu();
+        menu.menu();
     }
     
 }
